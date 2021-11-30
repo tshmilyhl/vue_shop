157e27c1258ce8145d4a5dbd172e9870be1632ab
tshmilyhl <985154610@qq.com>
2021-11-30 20:29:37 +0800

    completed Eslint&import by needing

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
-import  {Form,FormItem} from 'element-ui'
-import {Input } from 'element-ui'
+// import { Button } from 'element-ui'
+// import  {Form,FormItem} from 'element-ui'
+// import {Input } from 'element-ui'
 //弹窗组件
-import {Message} from "element-ui";
+// import {Button,} from 'element-ui'
+//按需导入
+import {Message,Form,FormItem,Input,Button} from "element-ui";
+
 
 Vue.use(Button)
 Vue.use(Form)
