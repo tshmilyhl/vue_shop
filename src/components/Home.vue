879553c38fcde7077cd7119fe8c3089577cebf36
tshmilyhl <985154610@qq.com>
2021-12-13 08:46:21 +0800

    完成了用户列表功能开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,142 @@
 <template>
-<div>
-  <el-button @click="logout" type="info">退出</el-button>
-</div>
+  <el-container class="home-container">
+<!--    头部区域-->
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt="">
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button @click="logout" type="info">退出</el-button>
+    </el-header>
+<!--    页面主体区域-->
+    <el-container>
+<!--      侧边栏-->
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse" >
+          |||
+        </div>
+<!--        侧边栏菜单区域-->
+        <el-menu
+            background-color="#333744"
+            text-color="#fff"
+            active-text-color="#409EFF"
+            :unique-opened="true"
+            :collapse="isCollapse"
+            :collapse-transition="false"
+            router
+            :default-active="activePath"
+        >
+<!--          一级菜单-->
+          <el-submenu :index="item.id +''" v-for="item in menulist" :key="item.id">
+<!--            一级菜单模板区域-->
+            <template slot="title">
+              <i :class="iconObj[item.id]"></i>
+              <span>{{item.authName}}</span>
+            </template>
+<!--            二级菜单-->
+            <el-menu-item :index="'/'+subItem.path" v-for="subItem in item.children"
+                          :key="subItem.id" @click="saveNavState('/'+ subItem.path)">
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>{{subItem.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+<!--      右侧内容主体-->
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
   name: "Home",
+  data(){
+    return {
+      //左侧菜单数据
+      menulist:[],
+      iconObj:{
+        '125':'iconfont icon-user',
+        '103':'iconfont icon-tijikongjian',
+        '101':'iconfont icon-shangpin',
+        '102':'iconfont icon-danju ',
+        '145':'iconfont icon-baobiao'
+      },
+      isCollapse:false,
+      // 被激活的链接地址
+      activePath:''
+    }
+  },
+  created(){
+    this.getMenuList()
+    this.activePath = window.sessionStorage.getItem('activePath')
+  },
   methods:{
     logout(){
       window.sessionStorage.clear()
       this.$router.push("/login")
+    },
+    async getMenuList(){
+      const {data:res} = await this.$http.get('menus')
+      if(res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menulist = res.data
+    },
+  //  点击按钮切换折叠与展开
+    toggleCollapse(){
+      this.isCollapse = ! this.isCollapse
+    },
+  //  保存链接的激活状态
+    saveNavState(activePath){
+      window.sessionStorage.setItem('activePath',activePath)
+      this.activePath = activePath
     }
   }
 }
 </script>
 
-<style scoped>
-
+<style lang="less" scoped>
+.el-header{
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+}
+.el-header div {
+  display: flex;
+  align-items: center;
+  span {
+    margin-left: 15px;
+  }
+}
+.el-aside{
+  background-color: #333744;
+  .el-menu {
+    border-right: none;
+  }
+}
+.el-main{
+  background-color: #eaedf1;
+}
+.home-container{
+  height: 100%;
+}
+.iconfont {
+  margin-right: 10px;
+}
+.toggle-button{
+  background-color: #4a5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -5,14 +5,36 @@ import Vue from 'vue'
 //弹窗组件
 // import {Button,} from 'element-ui'
 //按需导入
-import {Message,Form,FormItem,Input,Button} from "element-ui";
+import {Message,Form,FormItem,Input,
+    Button,Container,Header,Main,Aside,
+    Menu,Submenu,MenuItem,Breadcrumb,BreadcrumbItem,
+    Card,Row,Col,Table,TableColumn,Switch,Pagination,Dialog,Tooltip,MessageBox } from "element-ui";
 
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Main)
+Vue.use(Aside)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Col)
+Vue.use(Row)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Pagination)
+Vue.use(Dialog)
+Vue.use(Tooltip)
 
 //全局组件
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox .confirm
 
